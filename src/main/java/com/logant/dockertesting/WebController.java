a7eaf8d1496f865c1a1b85afc196781e3e0f118b
Purwatsate <ant.pwt@gmail.com>
2024-09-12 19:49:19 +0630

    up

--- src/main/java/com/logant/dockertesting/WebController.java
+++ src/main/java/com/logant/dockertesting/WebController.java
@@ -1,15 +1,25 @@
 package com.logant.dockertesting;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class WebController {
 
-    @RequestMapping("/hello-docker")
-    public String home() {
-        return "Welcome! Successfully Docker";
+    @GetMapping("/hello-docker")
+    public ResponseEntity<List<String>> hello() {
+        List<String> messages = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            messages.add("Welcome message " + i);
+        }
+        return ResponseEntity.ok(messages);
     }
 }
