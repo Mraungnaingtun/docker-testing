895f75b6388d301685944cde04699570bd152a51
Purwatsate <ant.pwt@gmail.com>
2024-09-12 19:36:00 +0630

    cross

--- src/main/java/com/logant/dockertesting/WebController.java
+++ src/main/java/com/logant/dockertesting/WebController.java
@@ -1,8 +1,10 @@
 package com.logant.dockertesting;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class WebController {
 
